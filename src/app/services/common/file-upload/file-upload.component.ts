a4205a9ff706cfb369417c4e8f19d442d228d644
umeyiratasoy <umeyiratasoyy@gmail.com>
2024-03-18 16:26:25 +0300

    Table Per Hierarchy Approach

--- src/app/services/common/file-upload/file-upload.component.ts
+++ src/app/services/common/file-upload/file-upload.component.ts
@@ -7,6 +7,8 @@ import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui
 import { MatDialog } from '@angular/material/dialog';
 import { FileUploadDialogComponent, FileUploadDialogState } from 'src/app/dialogs/file-upload-dialog/file-upload-dialog.component';
 import { DialogService } from '../dialog.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from 'src/app/base/base.component';
 
 @Component({
   selector: 'app-file-upload',
@@ -21,7 +23,8 @@ export class FileUploadComponent {
     private alertifyService:AlertifyService,
     private customToastrService:CustomToastrService,
     private dialog: MatDialog,
-    private dialogService:DialogService){}
+    private dialogService:DialogService,
+    private spinner:NgxSpinnerService){}
 
   public files: NgxFileDropEntry[];
 
@@ -42,6 +45,7 @@ export class FileUploadComponent {
       componentType: FileUploadDialogComponent,
       data:FileUploadDialogState.Yes,
       afterClosed: () => {
+        this.spinner.show(SpinnerType.BallAtom)
         this.httpClientService.post({
           controller:this.options.controller,
           action:this.options.action,
@@ -49,6 +53,8 @@ export class FileUploadComponent {
           headers:new HttpHeaders({"responseType": "blob"})
         },fileData).subscribe(data => {
           const message:string = "Dosyalar başarıyla yüklenmiştir";
+          
+        this.spinner.hide(SpinnerType.BallAtom)
           if(this.options.isAdminPage){
             this.alertifyService.message( message,  {
               dismissOthers: true,
@@ -62,6 +68,7 @@ export class FileUploadComponent {
               position:ToastrPosition.BottomRight
             })
           }
+          
     
         }, (errorResponse:HttpErrorResponse) => {
           const message:string = "Dosyalar yüklenirken beklenmeyen bir hata ile karşılaşılmıştır.";
