48e43ac7069d52e76db328d09216ffa14d42443e
umeyiratasoy <umeyiratasoyy@gmail.com>
2024-04-26 15:29:55 +0300

    Link Check-ReturnURL Function with IdentityCheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/common/auth.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
+import { Router } from '@angular/router';
 
 
 declare var $: any
@@ -10,9 +13,20 @@ declare var $: any
 })
 export class AppComponent {
   title = 'ETicaretClient';
-  constructor() {
+  constructor(public authService:AuthService, private toastrService:CustomToastrService, private router:Router ) {
 
+
+    authService.identityCheck();
   }
   
+  signOut() {
+    localStorage.removeItem("accessToken")
+    this.authService.identityCheck();
+    this.router.navigate([""]);
+    this.toastrService.message("Oturum Kapatılmıştır.", "Oturum Kapatılmıştır", {
+      messageType: ToastrMessageType.Warning,
+      position:ToastrPosition.BottomRight
+    })
+  }
 
 } 
--- src/app/guards/common/auth.guard.ts
+++ src/app/guards/common/auth.guard.ts
@@ -5,31 +5,33 @@ import { JwtHelperService } from '@auth0/angular-jwt'
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../services/ui/custom-toastr.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from '../../base/base.component';
+import { AuthService, _isAuthenticated } from 'src/app/services/common/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private jwtHelper: JwtHelperService, private router: Router, private toastrService: CustomToastrService, private spinner: NgxSpinnerService) {
+  constructor(private jwtHelper: JwtHelperService, private router: Router, private toastrService: CustomToastrService, private spinner: NgxSpinnerService
+  ) {
 
   }
   
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     this.spinner.show(SpinnerType.BallAtom);
-    const token: string = localStorage.getItem("accessToken");
+    // const token: string = localStorage.getItem("accessToken");
 
-    //const decodeToken = this.jwtHelper.decodeToken(token);
-    //const expirationDate: Date = this.jwtHelper.getTokenExpirationDate(token);
-    let expired : boolean;
-    try{
-      expired = this.jwtHelper.isTokenExpired(token);
-    }catch{
-      expired:true;
-    }
+    // //const decodeToken = this.jwtHelper.decodeToken(token);
+    // //const expirationDate: Date = this.jwtHelper.getTokenExpirationDate(token);
+    // let expired : boolean;
+    // try{
+    //   expired = this.jwtHelper.isTokenExpired(token);
+    // }catch{
+    //   expired:true;
+    // }
 
-    if(!token || expired) {
+    if(!_isAuthenticated) {
       this.router.navigate(["login"], {queryParams:{returnUrl:state.url}});
       this.toastrService.message("Oturum açmanız gerekiyor.", "Yetkisiz Erişim", {
         messageType:ToastrMessageType.Warning,
