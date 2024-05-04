71e054846b90008df90d9a9cc594b992e66b1527
umeyiratasoy <umeyiratasoyy@gmail.com>
2024-05-05 00:16:42 +0300

    SignalR Infrastructure Integration Architecturally

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
+
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HubUrls } from 'src/app/constants/hub-urls';
+import { ReceiveFunctions } from 'src/app/constants/receive-functions';
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { SignalRService } from 'src/app/services/common/signalr.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +14,18 @@ import { AlertifyService, MessageType, Position } from 'src/app/services/admin/a
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner:NgxSpinnerService) {
-    super(spinner);
-   }
+  constructor(private alertify: AlertifyService, spinner: NgxSpinnerService, private signalRService: SignalRService) {
+    super(spinner)
+    signalRService.start(HubUrls.ProductHub)
+  }
   
-  ngOnInit(): void {
-    
+   ngOnInit(): void {
+    this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
+      this.alertify.message(message, {
+        messageType: MessageType.Notify,
+        position: Position.TopRight
+      })
+    });
   }
 
 }
