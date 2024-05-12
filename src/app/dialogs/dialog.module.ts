4c73b48ab298b11273931aced3129b41bbe01694
umeyiratasoy <umeyiratasoyy@gmail.com>
2024-05-12 23:25:14 +0300

    Coding of Order Details and Coding of Mail Service

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -9,7 +9,9 @@ import { FileUploadModule } from '../services/common/file-upload/file-upload.mod
 import {MatCardModule} from '@angular/material/card';
 import { BasketItemRemoveDialogComponent } from './basket-item-remove-dialog/basket-item-remove-dialog.component';
 import { ShoppingCompleteDialogComponent } from './shopping-complete-dialog/shopping-complete-dialog.component';
-
+import { OrderDetailDialogComponent } from './order-detail-dialog/order-detail-dialog.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
@@ -19,14 +21,17 @@ import { ShoppingCompleteDialogComponent } from './shopping-complete-dialog/shop
     DeleteDialogComponent,
     SelectProductImageDialogComponent,
     BasketItemRemoveDialogComponent,
-    ShoppingCompleteDialogComponent
+    ShoppingCompleteDialogComponent,
+    OrderDetailDialogComponent
   ],
   imports: [
     CommonModule,
     MatDialogModule,
     MatButtonModule,
     MatCardModule,
+    MatTableModule, 
     FileUploadModule,
+    MatToolbarModule
     
   ]
 })
