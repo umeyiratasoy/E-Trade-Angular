08fddcab242f8e1848ae2394e375fc9f7dde6f60
umeyiratasoy <umeyiratasoyy@gmail.com>
2024-05-26 16:02:30 +0300

    Creating a RolePermissionFilter and Authorization Control

--- src/app/services/common/http-error-handler-interrceptor.service.ts
+++ src/app/services/common/http-error-handler-interrceptor.service.ts
@@ -36,6 +36,10 @@ export class HttpErrorHandlerInterrceptorService implements HttpInterceptor {
                 });
             }
           }).then(data => {
+            this.toastrService.message("Bu işlemi yapmaya yetkiniz bulunmamaktadır!", "Yetkisiz işlem!", {
+              messageType: ToastrMessageType.Warning,
+              position: ToastrPosition.BottomRight
+            });
 
           });
           break;
